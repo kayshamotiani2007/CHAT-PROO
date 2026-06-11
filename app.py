a071604537dca1c5545201537dccd621b7b304e6
kaysha motiani <kayshamotiani2007@gmail.com>
2026-06-11 22:39:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,20 @@ import streamlit as st
 import os 
 import google.generativeai as genai 
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+if "GOOGLE_API_KEY" in st.secrets:
+    api_key = st.secrets["GOOGLE_API_KEY"]
+else:
+    from dotenv import load_dotenv 
+    load_dotenv() 
+    api_key = os.getenv("GOOGLE_API_KEY")
+
+# Configure the API key safely
+if api_key:
+    genai.configure(api_key=api_key)
+else:
+    st.error("API Key not found! Please check your Streamlit Secrets or .env file.")
+
+
 model = genai.GenerativeModel("gemini-2.5-flash") 
 
 def my_output(query):
